27c12e1e30a845fbbbf18889fddf34d77b60b4c6
Ssakshii27 <93833003+Ssakshii27@users.noreply.github.com>
2022-07-07 21:15:35 +0530

    Add files via upload
    IT IS A WEB APPLICATION TO CONVERT ARABIC NUMBERS TO ROMAN NUMBERS.

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyA7yi4ABOANgCvAuwE2mIy1-ocwVwrBmnc",
+  authDomain: "roman-f79ab.firebaseapp.com",
+  databaseURL: "https://roman-f79ab-default-rtdb.firebaseio.com",
+  projectId: "roman-f79ab",
+  storageBucket: "roman-f79ab.appspot.com",
+  messagingSenderId: "557746458794",
+  appId: "1:557746458794:web:4cbfe55dab1f5e72a680d3",
+  measurementId: "G-9P0XQDZDH9"
+};
+firebase.initializeApp(firebaseConfig);
